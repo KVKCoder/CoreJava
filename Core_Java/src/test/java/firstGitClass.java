550cabb1dc546598049dbf87bbacc822f3c6f75e
KVKrishna <krishnakvbharadwaj@gmail.com>
2024-09-08 15:33:27 +0530

    Second commit

--- Core_Java/src/test/java/firstGitClass.java
+++ Core_Java/src/test/java/firstGitClass.java
@@ -5,6 +5,7 @@ public class firstGitClass {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Welcome Krishna and start using Git");
+                 System.out.println("Welcome Krishna and start using Github");
 
 	}
 
