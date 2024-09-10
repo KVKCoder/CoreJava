8e37de5bbbd54440905e1bc89bb5793f480575f8
KVKrishna <krishnakvbharadwaj@gmail.com>
2024-09-10 18:26:58 +0530

    Added test file

---
+++ Rest_Assured/src/test/java/apiTesting/Testclass.java
@@ -0,0 +1,12 @@
+package apiTesting;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi Welcome to Rest Assured");
+		
+	}
+
+}
